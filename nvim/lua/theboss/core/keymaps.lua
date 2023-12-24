8f82d940f4a715141ad149a549698fcbd7cf51d5
Nicolas Lauzon <nicolas_lauzon@outlook.com>
2023-12-24 04:08:28 -0500

    Yay nvim config done

--- nvim/lua/theboss/core/keymaps.lua
+++ nvim/lua/theboss/core/keymaps.lua
@@ -10,3 +10,7 @@ keymap.set("n", "<leader>-", "<C-x>") -- decrement
 keymap.set("n", "<C-p>", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
 keymap.set("n", "<leader>s", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
 keymap.set("n", "<leader>f", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
+
+-- nvim-tree
+keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>")
+
